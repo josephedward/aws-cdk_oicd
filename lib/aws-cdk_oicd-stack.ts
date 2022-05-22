cf9199d572c4aaa292b1b9cf653acafb8df5a499
JoeV <15126922+josephedward@users.noreply.github.com>
2022-05-22 00:32:46 -0400

    update

--- lib/aws-cdk_oicd-stack.ts
+++ lib/aws-cdk_oicd-stack.ts
@@ -1,7 +1,13 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
-import { OpenIdConnectProvider, OpenIdConnectPrincipal, Role, PolicyDocument, PolicyStatement, Effect } from "aws-cdk-lib/aws-iam";
-
+import {
+  OpenIdConnectProvider,
+  OpenIdConnectPrincipal,
+  Role,
+  PolicyDocument,
+  PolicyStatement,
+  Effect,
+} from "aws-cdk-lib/aws-iam";
 
 export class AwsCdkOicdStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -17,7 +23,7 @@ export class AwsCdkOicdStack extends Stack {
     });
 
     const githubOrganisation = "josephedward";
-    const repoName = "aws-cdk_oicd";
+    const repoName = "aws-cdk-example";
     /**
      * Create a principal for the OpenID; which can allow it to assume
      * deployment roles.
@@ -29,32 +35,30 @@ export class AwsCdkOicdStack extends Stack {
         },
       }
     );
-/**
-  * Create a deployment role that has short lived credentials. The only
-  * principal that can assume this role is the GitHub Open ID provider.
-  *
-  * This role is granted authority to assume aws cdk roles; which are created
-  * by the aws cdk v2.
-  */
- new Role(this, 'GitHubActionsRole', {
-  assumedBy: GitHubPrincipal,
-  description:
-    'Role assumed by GitHubPrincipal for deploying from CI using aws cdk',
-  roleName: 'github-ci-role',
-  inlinePolicies: {
-    CdkDeploymentPolicy: new PolicyDocument({
-      assignSids: true,
-      statements: [
-        new PolicyStatement({
-          effect: Effect.ALLOW,
-          actions: ['sts:AssumeRole'],
-          resources: [`arn:aws:iam::${this.account}:role/cdk-*`],
+    /**
+     * Create a deployment role that has short lived credentials. The only
+     * principal that can assume this role is the GitHub Open ID provider.
+     *
+     * This role is granted authority to assume aws cdk roles; which are created
+     * by the aws cdk v2.
+     */
+    new Role(this, "GitHubActionsRole", {
+      assumedBy: GitHubPrincipal,
+      description:
+        "Role assumed by GitHubPrincipal for deploying from CI using aws cdk",
+      roleName: "github-ci-role",
+      inlinePolicies: {
+        CdkDeploymentPolicy: new PolicyDocument({
+          assignSids: true,
+          statements: [
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ["sts:AssumeRole"],
+              resources: [`arn:aws:iam::${this.account}:role/cdk-*`],
+            }),
+          ],
         }),
-      ],
-    }),
-  },
-});
-    
+      },
+    });
   }
-  
 }
--- test/aws-cdk_oicd.test.ts
+++ test/aws-cdk_oicd.test.ts
@@ -2,8 +2,6 @@
 // import { Template } from 'aws-cdk-lib/assertions';
 // import * as AwsCdkOicd from '../lib/aws-cdk_oicd-stack';
 
-// example test. To run these tests, uncomment this file along with the
-// example resource in lib/aws-cdk_oicd-stack.ts
 test('SQS Queue Created', () => {
 //   const app = new cdk.App();
 //     // WHEN
